83f0ae0dced518313eb2b850aed83625a0e280a1
fabri <fguespe@gmail.com>
2024-08-11 17:07:45 -0300

    update

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-import { run, HandlerContext } from "@xmtp/botkit";
+import { run, HandlerContext } from "@xmtp/message-kit";
 
 //Tracks conversation steps
 const inMemoryCacheStep = new Map<string, number>();
@@ -7,41 +7,42 @@ const inMemoryCacheStep = new Map<string, number>();
 const stopWords = ["stop", "unsubscribe", "cancel", "list"];
 
 run(async (context: HandlerContext) => {
-  const { content, senderAddress } = context.message;
-  const lowerContent = content.toLowerCase();
+  const { content, sender } = context.message;
+  const { content: text } = content;
 
+  const senderAddress = sender.address;
   //Handles unsubscribe and resets step
-  if (stopWords.some((word) => lowerContent.includes(word))) {
+  if (stopWords.some((word) => text.toLowerCase().includes(word))) {
     inMemoryCacheStep.set(senderAddress, 0);
   }
 
   const cacheStep = inMemoryCacheStep.get(senderAddress) || 0;
-  let message = "";
+
   if (cacheStep === 0) {
     // send the first message
-    await context.reply(
+    await context.send(
       "Welcome to The General Store powered by ENS + XMTP, where web3 builders can get supplies, anytime, day or night."
     );
     // send the second message
-    await context.reply(
+    await context.send(
       "Below is our menu. Let us know the number of the item you want, and it's yours. If it's a digital good, our bot will deliver those items right to your wallet.\n\nMenu:\n1. Chewing Gum\n2. TicTacs\n3. Chapstick\n4. RedBull\n5. Iced Coffee\n\n✍️ (reply with the number of the item you want)"
     );
 
-    await context.reply(
+    await context.send(
       "Want faucets? Head to faucetbot.eth : Delivers Faucet funds to devs on Testnet"
     );
 
     inMemoryCacheStep.set(senderAddress, 1);
   } else if (cacheStep === 1) {
     const validOptions = ["1", "2", "3", "4", "5"];
-    if (!validOptions.includes(content)) {
-      await context.reply(
+    if (!validOptions.includes(text)) {
+      await context.send(
         "Invalid option selected. Please enter a valid option (1, 2, 3, 4, or 5)\n\nIf you'd like to restart the bot, you can do so at any time by saying 'stop'."
       );
       return;
     }
 
-    await context.reply(
+    await context.send(
       "Your order was successfully placed. Thank you for shopping with us!"
     );
 
