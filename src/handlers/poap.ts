9463930f2010defcb674fccbd919008ebed8355d
fabri <fguespe@gmail.com>
2024-10-22 10:05:27 -0300

    update

--- src/handlers/poap.ts
+++ src/handlers/poap.ts
@@ -1,9 +1,10 @@
-import { HandlerContext } from "@xmtp/message-kit";
-import { clearChatHistory } from "./agent.js";
+import { HandlerContext, xmtpClient } from "@xmtp/message-kit";
 import { db } from "../lib/db.js";
 await db.read();
 
 export async function handlePoap(context: HandlerContext) {
+  //@ts-ignore
+  const { name } = context;
   const {
     message: {
       content: { content: text, command, params },
@@ -14,35 +15,38 @@ export async function handlePoap(context: HandlerContext) {
   if (command == "poap" && text == "/poap list") {
     const poapTable = db?.data?.poaps;
     const claimed = poapTable.filter((poap) => poap.Address);
-    await context.send(
-      `You have claimed ${claimed.length} POAPs out of ${poapTable.length}`
-    );
+    return {
+      code: 200,
+      message: `You have claimed ${claimed.length} POAPs out of ${poapTable.length}`,
+    };
   } else if (command == "poap") {
     // Destructure and validate parameters for the ens command
     const { address } = params;
-
-    if (!address) {
-      await context.send(
-        "Missing required parameters. Please provide address."
-      );
-      return;
-    }
     await db.read();
     const poapTable = db?.data?.poaps;
     const poap = poapTable.find((poap) => poap.Address == address);
-    console.log(poap);
+
     if (!poap) {
       const emptyPoap = poapTable.find((poap) => !poap.Address);
       if (emptyPoap) {
         db?.data?.poaps?.push({ URL: emptyPoap?.URL, Address: address });
-        await context.send(`Here is your POAP ${emptyPoap?.URL}`);
-        await db.write();
+        //?user_address=${address}`
+        return {
+          code: 200,
+          message: `Here is your POAP ${emptyPoap?.URL}`,
+        };
       } else {
-        await context.send("No more POAPs available");
+        return {
+          code: 200,
+          message: "No more POAPs available",
+        };
       }
     } else {
-      await context.send(`You have already claimed this POAP ${poap?.URL}`);
+      //?user_address=${address}`
+      return {
+        code: 200,
+        message: `You have already claimed this POAP ${poap?.URL}`,
+      };
     }
-    //clearChatHistory();
   }
 }
